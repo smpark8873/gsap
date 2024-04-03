886519ab183852326376f428e575ba47eac916d8
smpark8873 <smpark@banaple.co.kr>
2024-04-03 16:44:36 +0900

    page 수정

--- src/pages/TestScroll.jsx
+++ src/pages/TestScroll.jsx
@@ -1,4 +1,5 @@
-import React, { useRef } from 'react';
+import * as React from 'react';
+import { Reset } from 'styled-reset';
 import gsap from "gsap"; 
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -6,66 +7,36 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import '../test.css';
 
 function TestScroll () { 
-    // gsap.registerPlugin(ScrollTrigger);
+    gsap.registerPlugin(ScrollTrigger);
 
-    // const marqueeRef = useRef(null);
 
    
     useGSAP(() => {
-        // const marqueeElement = marqueeRef.current;
-        // const marqueeWidth = marqueeElement.offsetWidth;
-        // const duration = marqueeWidth / 150;
-    
-
-        // const tl = gsap.timeline({ repeat: -1 });
-        // tl.to(marqueeElement, { x: -marqueeWidth, duration, ease: 'none' });
-
+       
       
-        var colors = ["#f38630","#6fb936", "#ccc", "#6fb936"];
-
-        //initially colorize each box and position in a row
-        gsap.set(".box", {
-          backgroundColor: (i) => colors[i % colors.length],
-          x: (i) => i * 50
-        });
-        
-        
-        gsap.to(".box", {
-          duration: 5,
-          ease: "none",
-          x: "+=500", //move each box 500px to right
-          modifiers: {
-            x: gsap.utils.unitize(x => parseFloat(x) % 500) //force x value to be between 0 and 500 using modulus
-          },
-          repeat: -1
-        });      
 
         
           
       
     }, {});    
      
-    return (         
-    //    <div className="wrapper">
-    //         <div className='section01'>
-    //             <p ref={marqueeRef}>BANAPRESSO BANAPRESSO</p>
-    //             <div className='coffee'><img src="/img/testScroll/coffee.png" alt="" /></div>
-    //             <p ref={marqueeRef}>BANAPRESSO BANAPRESSO</p>
-    //         </div>
-    //    </div>
+    return ( 
     <div class="wrapper">
-        <div class="boxes">
-            <div class="box">1</div>
-            <div class="box">2</div>
-            <div class="box">3</div>
-            <div class="box">4</div>
-            <div class="box">5</div>
-            <div class="box">6</div>
-            <div class="box">7</div>
-            <div class="box">8</div>
-            <div class="box">공부할 시간이 없다</div>
-            <div class="box"></div>
-        </div>
+        <section className='page page01'>
+          <h1 className='logo'>ABCDEFGHIJKLMNOPQRSTUVWXYZ</h1>
+        </section>
+        <section className='page page02'>
+        </section>
+        <section className='page page03'>
+          
+        </section>
+        <section className='page page04'>
+          
+        </section>
+        <section className='page page05'>
+          
+        </section>
+       
     </div>
    );
 }
